16a230921e4139584a7ec74f12a4a8513c1a356d
Bullwhips <epicj4444@gmail.com>
2025-04-28 14:30:35 +0200

    Path changes

---
+++ server.js
@@ -0,0 +1,69 @@
+import { serveDir, serveFile } from "jsr:@std/http/file-server";
+
+
+let GRUPPER = []
+
+
+const grupperData = await Deno.readTextFile("./game/database/database.json")
+GRUPPER = JSON.parse(grupperData)
+
+
+async function updateDatabase(grupper) {
+    const grupperData = JSON.stringify(grupper, null, 2)
+    await Deno.writeTextFile("./game/database/database.json", grupperData)
+}
+
+
+async function handleRequest(request) {
+    const pathname = new URL(request.url).pathname
+
+
+    if(pathname.startsWith("/static")) {
+            return serveDir(request, {
+                fsRoot: "game",
+                urlRoot: "static"
+            })
+        }
+
+
+            if (pathname == "/api/grupper")
+                {
+                    const options = {
+                        headers: {"Content-Type": "application/json"}
+                    }
+                    if (request.method == "GET") {
+                        return new Response(JSON.stringify(GRUPPER), options)
+                    }
+
+
+                    if (request.method == "POST") {
+                        const grupp = await request.json()
+                        grupp.id = GRUPPER.reduce((acc, next) => next.id > acc ? acc = next.id : acc, 0) + 1
+                        grupp.time = ""
+                        GRUPPER.push(grupp)
+                        updateDatabase(GRUPPER)
+                        return new Response(JSON.stringify(grupp), options)
+                    }
+
+
+                    if (request.method == "DELETE") {
+                       
+                        const data = await request.json()
+                        const grupp = GRUPPER.find(grupp => grupp.id == data.id)
+                       if (grupp == undefined) {
+                        return new Response("", {status: 404})
+                       }
+
+
+                        GRUPPER = GRUPPER.filter((grupp) => grupp.id != data.id)
+                        updateDatabase(GRUPPER)
+                        return new Response(JSON.stringify(grupp), options)
+                    }
+                }
+                return serveFile(request, "./game/index.html")
+        }
+
+
+
+
+Deno.serve(handleRequest)
