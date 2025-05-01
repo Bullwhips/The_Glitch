8893a4d4371a6db49b422d6a89e5622412c1a0af
mikaelarasmusson <143449047+mikaelarasmusson@users.noreply.github.com>
2025-05-01 20:02:27 +0200

    change textcontent, changed some css

--- game/components/page_1/page_1.js
+++ game/components/page_1/page_1.js
@@ -20,35 +20,30 @@ function renderPage1(wrapper) {
 
     let welcomeText1 = document.createElement("h2")
     welcomeText1.id = "welcomeText1"
-    welcomeText1.textContent= "VÄLKOMMEN SPELARE. VARJE DETALJ ÄR VIKTIG, HÅLL ERA ÖGON OCH ÖRON ÖPPNA"
+    welcomeText1.textContent= "VÄLKOMNA SPELARE"
     textContainer.append(welcomeText1)
 
     let textPage1 = document.createElement("p")
     textPage1.id = "textPage1"
     textPage1.innerHTML =
-                 `Ni har nu fått se en introduktions film där Kaira har fastnat i glitchen.
-                  Detta betyder att ni har tagit er igenom första delen och spelet är officiellt igång.
-                  Som ni kan se uppe i höger hörn så går spelet på tid.
-                  Tiden går inte att pausa eller stanna på något sätt. 
-                  Det betyder att när tiden är ute så avslutas spelet oavsett om ni har klarat spelet eller inte, 
-                  därför är det extra viktigt att ni nu är uppmärksamma på allt runt omkring er och var inte blyga för att anteckna. <br><br>
-                  Under klockan på höger sida hittar ni en meny. Högst upp i menyn finner ni spelregler, 
-                  i mitten finner ni olika medier som kan behövas och längst ner hittar ni information om oss och kontaktuppgifter om ni stöter på problem i spelet. <br><br>
-                  Var inte rädda för att testa er fram, klicka er genom appen och utforska. Om ni har tur så kan det under spelets gång dyka upp ledtrådar som kan vara användbara. 
-                  Lycka till...`
-    textContainer.append(textPage1)
+                 `Kaira har fastnat i en glitch. Nu är det upp till er att hjälpa henne. 
+                  Uppe i höger hörn ser ni tiden ni har på er att klara spelet. 
+                  Tiden går inte att pausa eller stanna på något sätt vilket betyder att när tiden är ute så är spelet slut. 
+                  Spelet kommer att avslutas oavsett hur långt ni kommit. 
+                  Det är därför extra viktigt att ni är uppmärksamma och snabbtänkta. 
+                  Samarbeta, för anteckningar och skapa samband. 
+                  Framtiden för rätt ögonblick vilar i era händer. <br><br>
+                  Under klockan på höger sida hittar ni en meny. 
+                  Högst upp i menyn finns spelregler, i mitten olika medier som kan behövas under spelets gång och längst ner hittar ni information om oss och kontaktuppgifter om ni skulle stöta på problem i spelet. <br><br>
+                  Var inte rädda för att testa er fram, klicka er gärna genom appen och utforska. Om ni har tur så kan det under spelets gång dyka upp ledtrådar som kan vara användbara. Lycka till...<br><br> 
+                  ALLA DETALJER ÄR VIKTIGA. DEN SOM SER ALLT MISSAR INGET.`;
+    textContainer.append(textPage1);
 
-
-    
-    
     let stepTwoButton = document.createElement("button")
     stepTwoButton.id = "stepTwoButton"
     stepTwoButton.textContent = "STEG 2"
     stepTwoButton.addEventListener("click", () => {renderPage2(wrapper)});
     textContainer.append(stepTwoButton)
 
-
     pageContainer.append(opacityBackground);
-
-
 }
